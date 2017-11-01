76d3a17a24913f083dd4c7c559ea061e4db5221f
luk3yx <luk3yx@users.noreply.github.com>
2017-11-02 08:21:15 +1300

    Code updates, and bugfixes

--- btbp.py
+++ btbp.py
@@ -22,12 +22,12 @@ def btbp(bot, trigger):
     
     elif cmd == 2: # MODE
         if len(n) < 4 or not n[1].startswith('#'):
-            btbpreply(meta, "202 false :Bad parameters for setmode.")
+            btbpreply(meta, "102 :Bad parameters for setmode.")
         elif 'admin' in privs or 'mode/' + n[2] in privs:
             bot.write(("MODE", n[1], n[2], n[3]))
-            btbpreply(meta, "202 true :Done!")
+            btbpreply(meta, "202 :Done!")
         else:
-            btbpreply(meta, "202 false :Permission denied.")
+            btbpreply(meta, "102 :Permission denied.")
     
     elif cmd == 0: # PING
         btbpreply(meta, "200 PONG {}".format(str.join(' ', n[1:])))
@@ -35,11 +35,11 @@ def btbp(bot, trigger):
     elif cmd == 5: # VERSION
         btbpreply(meta, "205 :Sopel/6.5.0")
     
-    elif cmd == 1: # PREFIX
+    elif cmd == 4: # PREFIX
         btbpreply(meta, "201 :.")
     
-    else:
-        btbpreply(meta, "101 :Invalid command: {}".format(cmd))
+    elif cmd < 100:
+        btbpreply(meta, "101 :Invalid command: {}".format(str(cmd).zfill(3)))
 
 def btbpreply(meta, message):
     meta[0].notice("\u0001BTBP {}\u0001".format(message), meta[1].nick)
