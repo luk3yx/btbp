9ab3ee16ca72b8da825ab60108a8d7109aba373e
luk3yx <luk3yx@users.noreply.github.com>
2017-11-01 16:35:56 +1300

    More numerical codes

--- btbp.py
+++ btbp.py
@@ -5,7 +5,7 @@ def btbp(bot, trigger):
             raise NameError()
         cmd = int(n[0])
     except:
-        btbpreply(bot, "throw mismatch Protocol mismatch.")
+        btbpreply(bot, "100: Protocol mismatch.")
         return
     global btbpver
     global trustedbots
@@ -14,31 +14,31 @@ def btbp(bot, trigger):
     else:
         privs = []
     if   cmd == 1: # PRIVS
-        btbpreply(bot, "privs {}".format(privs))
+        btbpreply(bot, "001 :{}".format(privs))
     
-    elif cmd == 'hostmask':
-        btbpreply(bot, "hostmask {}".format(trigger.hostmask))
+    elif cmd == 3:
+        btbpreply(bot, "003 :{}".format(trigger.hostmask))
     
     elif cmd == 2: # MODE
         if len(n) < 4 or not n[1].startswith('#'):
-            btbpreply(bot, "setmode invalidparams Bad parameters for setmode.")
+            btbpreply(bot, "002 false :Bad parameters for setmode.")
         elif 'admin' in privs or n[2] in privs:
             bot.write(("MODE", n[1], n[2], n[3]))
-            btbpreply(bot, "setmode done Done!")
+            btbpreply(bot, "002 true :Done!")
         else:
-            btbpreply(bot, "setmode denied Permission denied.")
+            btbpreply(bot, "002 false :Permission denied.")
     
-    elif cmd == 0:
-        btbpreply(bot, "0 PONG {}".format(str.join(' ', n)))
+    elif cmd == 0: # PING
+        btbpreply(bot, "000 PONG {}".format(str.join(' ', n)))
     
     elif cmd == 5: # VERSION
-        btbpreply(bot, "005 Sopel/6.5.0")
+        btbpreply(bot, "005 :Sopel/6.5.0")
     
     elif cmd == 1: # PREFIX
-        btbpreply(bot, "001 .")
+        btbpreply(bot, "001 :.")
     
     else:
-        btbpreply(bot, "throw invalidcmd Invalid command: {}".format(cmd))
+        btbpreply(bot, "101 :Invalid command: {}".format(cmd))
 
 def btbpreply(bot, message):
     bot.notice("\u0001BTBP {}\u0001".format(message))
