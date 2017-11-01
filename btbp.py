e1898826b303fab890844d54aea57b988efdbbb0
luk3yx <luk3yx@users.noreply.github.com>
2017-11-01 17:27:26 +1300

    CTCP Replies are now sent correctly

--- btbp.py
+++ btbp.py
@@ -9,36 +9,37 @@ def btbp(bot, trigger):
         return
     global btbpver
     global trustedbots
+    meta = [bot, trigger]
     if trigger.host in trustedbots.keys():
         privs = trustedbots[trigger.host]
     else:
         privs = []
     if   cmd == 1: # PRIVS
-        btbpreply(bot, "201 :{}".format(privs))
+        btbpreply(meta, "201 :{}".format(privs))
     
     elif cmd == 3:
-        btbpreply(bot, "203 :{}".format(trigger.hostmask))
+        btbpreply(meta, "203 :{}".format(trigger.hostmask))
     
     elif cmd == 2: # MODE
         if len(n) < 4 or not n[1].startswith('#'):
-            btbpreply(bot, "202 false :Bad parameters for setmode.")
+            btbpreply(meta, "202 false :Bad parameters for setmode.")
         elif 'admin' in privs or 'mode/' + n[2] in privs:
             bot.write(("MODE", n[1], n[2], n[3]))
-            btbpreply(bot, "202 true :Done!")
+            btbpreply(meta, "202 true :Done!")
         else:
-            btbpreply(bot, "202 false :Permission denied.")
+            btbpreply(meta, "202 false :Permission denied.")
     
     elif cmd == 0: # PING
-        btbpreply(bot, "200 PONG {}".format(str.join(' ', n[1:])))
+        btbpreply(meta, "200 PONG {}".format(str.join(' ', n[1:])))
     
     elif cmd == 5: # VERSION
-        btbpreply(bot, "205 :Sopel/6.5.0")
+        btbpreply(meta, "205 :Sopel/6.5.0")
     
     elif cmd == 1: # PREFIX
-        btbpreply(bot, "201 :.")
+        btbpreply(meta, "201 :.")
     
     else:
-        btbpreply(bot, "101 :Invalid command: {}".format(cmd))
+        btbpreply(meta, "101 :Invalid command: {}".format(cmd))
 
-def btbpreply(bot, message):
-    bot.notice("\u0001BTBP {}\u0001".format(message))
+def btbpreply(meta, message):
+    meta[0].notice("\u0001BTBP {}\u0001".format(message), meta[1].nick)
