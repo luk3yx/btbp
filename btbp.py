00effbcd8a703775ce7aa5b9643cb3ede05509b6
luk3yx <luk3yx@users.noreply.github.com>
2017-11-05 16:21:22 +1300

    Mass changes

--- btbp.py
+++ btbp.py
@@ -1,48 +1,81 @@
 from numerics import *
-from sopel import module
+#from sopel import module
 
 def btbp(bot, trigger):
     n = trigger.split(" ")
-    meta = [bot, trigger]
+    meta = [bot, trigger, 100]
     try:
         if len(n) < 1:
             raise NameError()
         cmd = int(n[0])
     except:
-        btbpreply(meta, "100 :Protocol mismatch.")
+        btbpreply(meta)
         return
+    meta[2] = cmd
     global btbpver
     global trustedbots
     if trigger.host in trustedbots.keys():
         privs = trustedbots[trigger.host]
     else:
         privs = []
-    if   cmd == 1: # PRIVS
-        btbpreply(meta, "201 :{}".format(privs))
+    if   cmd == CMD_PRIVS:
+        btbpreply(meta, privs)
     
-    elif cmd == 3:
-        btbpreply(meta, "203 :{}".format(trigger.hostmask))
+    elif cmd == CMD_HOSTMASK:
+        btbpreply(meta, trigger.hostmask)
     
-    elif cmd == 2: # MODE
+    elif cmd == CMD_MODE:
         if len(n) < 4 or not n[1].startswith('#'):
-            btbpreply(meta, "102 :Bad parameters for setmode.")
+            btbpreply(meta, [ERR_BAD_PARAMS])
         elif 'admin' in privs or 'mode' in privs or 'mode/' + n[2] in privs:
             bot.write(("MODE", n[1], n[2], n[3]))
-            btbpreply(meta, "202 :Done!")
+            btbpreply(meta, RES_DONE)
         else:
-            btbpreply(meta, "102 :Permission denied.")
+            btbpreply(meta, [ERR_PERM_DENIED, 'mode/' + n[2]])
     
-    elif cmd == 0: # PING
-        btbpreply(meta, "200 PONG {}".format(str.join(' ', n[1:])))
+    elif cmd == CMD_PING:
+        args = str.join(' ', n[1:])
+        if args == '':
+            args = ':PONG'
+        btbpreply(meta, args)
     
-    elif cmd == 5: # VERSION
-        btbpreply(meta, "205 :Sopel/6.5.0")
+    elif cmd == CMD_VERSION:
+        btbpreply(meta, "Sopel/6.5.0")
     
-    elif cmd == 4: # PREFIX
-        btbpreply(meta, "201 :.")
+    elif cmd == CMD_PREFIX:
+        btbpreply(meta, ".")
+    
+    elif cmd == CMD_UNIX:
+        if len(n) < 2:
+            btbpreply(meta, [ERR_BAD_PARAMS])
     
     elif cmd < 100:
-        btbpreply(meta, "101 {} :Invalid command".format(str(cmd).zfill(3)))
+        btbpreply(meta, [ERR_INV_COMMAND])
 
-def btbpreply(meta, message):
-    meta[0].notice("\1BTBP {}\1".format(message), meta[1].nick)
+def btbpreply(meta, message = None):
+    if len(meta) < 3:
+        meta[2] = 100
+    responses = {
+        ERR_PROTO_MISMATCH: ":Protocol mismatch.",
+        ERR_INV_COMMAND: "{} :Invalid command.",
+        ERR_BAD_PARAMS:  "{} :Bad parameters.",
+        ERR_PERM_DENIED: "{} :Permission denied.",
+        
+        RES_PRIV_LIST:   ":{}",
+        RES_DONE:        "{} :Done!",
+        RES_HOSTMASK:    ":{}",
+        RES_PREFIX:      ":{}",
+        RES_VERSION:     ":{}",
+    }
+    if type(message) == list and message[0] == True and len(message) > 0:
+        n = message[0]
+        if len(message) > 1:
+            meta[2] = '{} {}'.format(meta[2], message[1])
+        message = meta[2]
+        meta[2] = n
+        del n
+    elif not message or type(message) != str:
+        message = ''
+    if cmd in responses.keys():
+        message = responses[cmd].format(message)
+    meta[0].notice("\1BTBP {} {}\1".format(str(meta[2].zfill(3)), message), meta[1].nick)
--- numerics.py
+++ numerics.py
@@ -6,19 +6,21 @@ CMD_MODE           =   2     # Syntax: 002 #<channel> <mode> <args...>
 CMD_HOSTMASK       =   3     # Syntax: 003
 CMD_PREFIX         =   4     # Syntax: 004
 CMD_VERSION        =   5     # Syntax: 005
+CMD_UNIX           =   6     # Syntax: 006 <command>
 
 # [ERR] {1xx,} Error Numerics
 
-ERR_PROTO_MISMATCH = 100     # Format: 100 :Protocol mismatch
-ERR_INV_COMMAND    = 101     # Format: 101 <CMD> :Invalid command
-ERR_BAD_PARAMS     = 102     # Format: 102 <CMD> :Bad parameters
-ERR_PERM_DENIED    = 103     # Format: 103 <CMD> <MISSING_PRIVS> :Permission denied
+ERR_PROTO_MISMATCH = 100     # Format: 100 :Protocol mismatch.
+ERR_INV_COMMAND    = 101     # Format: 101 <CMD> :Invalid command.
+ERR_BAD_PARAMS     = 102     # Format: 102 <CMD> :Bad parameters.
+ERR_PERM_DENIED    = 103     # Format: 103 <CMD> <MISSING_PRIVS> :Permission denied.
 
 # [RES] {2xx,} Response Numerics
 
-RES_PONG           = 200     # Format: 200 :PONG
+RES_PONG           = 200     # Format: 200 <args OR "PONG">
 RES_PRIV_LIST      = 201     # Format: 201 :[{privs1}{,privs2}{...}{,privsN}]
 RES_DONE           = 202     # Format: 202 <CMD> :Done!
-RES_HOSTMASK       = 203     # Format: 203 nick!user@host :You are
-RES_PREFIX         = 204     # Format: 204 <PREFIX> :Is
+RES_HOSTMASK       = 203     # Format: 203 nick!user@host
+RES_PREFIX         = 204     # Format: 204 :<PREFIX>
 RES_VERSION        = 205     # Format: 205 :<VERSION>
+RES_UNIX           = 206     # Format: 206 <command> :<result>
